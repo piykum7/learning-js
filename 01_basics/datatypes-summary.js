abc828c334beae7f1d9544c9ec696146ade6ae45
piykum7 <108881774+piykum7@users.noreply.github.com>
2024-01-09 10:40:22 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,27 @@
+// primitive datatypes
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, Bigint
+
+
+
+//reference (Non premitive)
+
+// Array, Objects, Functions
+
+let id = Symbol("123")
+let id2 = Symbol("123")
+//console.log(id2);
+
+const heros = ["shaktiman", "superman", "batman"]
+
+let obj1 = {
+    name: "aman",
+    age: 22,
+    sex: "male"
+}
+
+const myfunction = function(){
+    console.log("hello");
+}
+
+console.log(typeof(myfunction));
