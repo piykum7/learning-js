df5e9622c31e955fbdb8a217d07eb317b3c7b833
piykum7 <108881774+piykum7@users.noreply.github.com>
2024-01-09 13:14:14 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,27 @@
+const score = 400
+const bal = new Number(100)
+
+//console.log(score);
+//console.log(bal);
+
+//console.log(bal.toString());
+
+//console.log(bal.toString().length);
+
+//console.log(bal.toFixed(2));
+
+const num1 = 2313.889
+//console.log(num1.toPrecision(3));
+
+const num2 = 1000000
+//console.log(num2.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//console.log(Math.abs(-5));
+
+console.log(Math.round(5.1));
+console.log(Math.ceil(5.1));
+console.log((Math.random()*10) + 1);
+
+
