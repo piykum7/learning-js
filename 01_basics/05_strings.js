554a3971b3debfbf36e51b6dd2b80480e7fbad4f
piykum7 <108881774+piykum7@users.noreply.github.com>
2024-01-09 11:35:43 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,29 @@
+const name = "amansinha"
+const gender = "male"
+
+//console.log(name + gender + "value");
+
+//console.log(`my name is ${name}`);
+
+const gamename = new String('aman-sinha-saint-number-1')
+//console.log(gamename[0]);
+//console.log(gamename.__proto__);
+
+//console.log(gamename.charAt(1));
+//console.log(gamename.toUpperCase());
+
+const str = gamename.substring(0,2)
+//console.log(str);
+
+const str2 = gamename.slice(1,3)
+//console.log(str2);
+
+const str0 = gamename
+//console.log(str0.trim());
+
+
+const url = "googlet"
+//console.log(url.replace('t', 'p'));
+//console.log(url.includes('yun'));
+
+console.log(gamename.split('-'));
