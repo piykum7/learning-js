61ead48eb45c865a3d2609555abe9739fec11cee
piykum7 <108881774+piykum7@users.noreply.github.com>
2024-01-09 06:56:42 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "abc.com"
+var accountpassword = "12345"
+
+
+//accountId=2
+accountEmail = "asdfgh"
+accountpassword = "3232323"
+let accountCity;
+
+//console.log(accountEmail);
+console.table([accountId,accountEmail,accountpassword,accountCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//alert("hello") //using node js not browser
+
+let name = "aman"
+let age = 28
+let logging = false
+
+//number =>2 to power 53
+//bigint
+//null => standalone value
+//undefined =>
+// symbol =>uniqu
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
