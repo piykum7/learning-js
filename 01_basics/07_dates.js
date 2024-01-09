3c4093f6745e792759fb97ab97c4a770227e6933
piykum7 <108881774+piykum7@users.noreply.github.com>
2024-01-09 15:16:31 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,31 @@
+//dates
+
+let mydate = new Date()
+
+//console.log(mydate.toString());
+//console.log(mydate.toDateString());
+//console.log(mydate.toLocaleString());
+//console.log(typeof(mydate));
+
+
+let mydate2 = new Date(2024,0,20,5,3)
+//console.log(mydate2.toLocaleString());
+
+let mydate1 = new Date(2024,0,20)
+//console.log(mydate1.toDateString());
+
+let mydate3 = new Date("2024-2-26")
+//console.log(mydate3.toLocaleString());
+
+let mydate4 = new Date("11-20-2023")
+//console.log(mydate4.toLocaleString());
+
+let timestamp = Date.now()
+
+//console.log(timestamp);
+//console.log(mydate4.getTime());
+//console.log(Date.now()/1000);
+
+let newdate = new Date()
+console.log(newdate);
+console.log(newdate.getHours());
