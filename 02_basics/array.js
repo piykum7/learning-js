0daa61ff7772ec90e7c7ba1e26e4f6dfa0881336
piykum7 <108881774+piykum7@users.noreply.github.com>
2024-01-09 17:37:19 +0000

    arrays

---
+++ 02_basics/array.js
@@ -0,0 +1,32 @@
+//array
+
+const arr = [1, 2, 3, 4, 5, "aman", true]
+const arr2 = new Array(1,3,5,7)
+
+//console.log(arr.length);
+
+//Array Methods
+
+arr2.push(19)
+arr2.pop()
+//console.log(arr2);
+
+//arr2.unshift(20)
+//console.log(arr2);
+
+//arr2.shift()
+//console.log(arr2);
+
+//console.log(arr2.includes(1));
+//console.log(arr2.indexOf(56));
+
+const arr3 = arr2.join()
+//console.log(arr3);
+//console.log(typeof arr3);
+//console.log(arr2);
+
+//slice, splice
+
+//console.log(arr2.slice(1, 3));
+console.log("B ", arr2);
+console.log(arr2.splice(1, 3));
